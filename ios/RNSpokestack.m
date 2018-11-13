f7566cbd6b3409afc9511fd74ccac4669616109d
Noel Weichbrodt <noel@weichbrodt.org>
2018-11-13 11:06:15 -0500

    WIP

--- ios/RNSpokestack.m
+++ ios/RNSpokestack.m
@@ -5,55 +5,65 @@
 
 @implementation RNSpokestack
 {
-  bool hasListeners;
+    bool hasListeners;
 }
 
 RCT_EXPORT_MODULE();
 
 - (dispatch_queue_t)methodQueue
 {
-  return dispatch_get_main_queue();
+    return dispatch_get_main_queue();
 }
 
 -(void)startObserving
 {
-  hasListeners = YES;
+    hasListeners = YES;
 }
 -(void)stopObserving
 {
-  hasListeners = NO;
+    hasListeners = NO;
 }
 
 - (NSArray<NSString *> *)supportedEvents
 {
-  return @[@"SpeechEvent"];
+    return @[@"SpeechEvent"];
 }
 
 - (void)onEvent:(NSNotification *)speechEvent
 {
-  NSString *eventName = speechEvent.userInfo[@"name"];
-  if (hasListeners)
-  {
-    RCTLogInfo(@"Pretending to event %@", eventName);
-    [self sendEventWithName:@"SpeechEvent" body:@{@"name": eventName}];
-  }
+    NSString *eventName = speechEvent.userInfo[@"name"];
+    if (hasListeners)
+    {
+        RCTLogInfo(@"Pretending to event %@", eventName);
+        [self sendEventWithName:@"SpeechEvent" body:@{@"name": eventName}];
+    }
 }
 
-// @property (nonatomic) SpeechPipeline* pipeline;
+SpeechPipeline* _pipeline;
 
 RCT_EXPORT_METHOD(initialize:(NSDictionary *)config)
 {
-  RCTLogInfo(@"Pretending to initialize with config %@", config);
+    RCTLogInfo(@"Pretending to initialize with config %@", config);
+    GoogleRecognizerConfiguration *_recognizerConfig = [[GoogleRecognizerConfiguration alloc] init];
+    _recognizerConfig.host = config["host"];
+//    _recognizerConfig.enableWordTimeOffsets = config["enableWordTimeOffsets"];
+//    _recognizerConfig.singleUtterance = config["singleUtterance"];
+//    _recognizerConfig.maxAlternatives = config["maxAlternatives"];
+//    _recognizerConfig.interimResults = config["interimResults"];
+//    _recognizerConfig.apiKey = config["apiKey"];
+//    _pipeline = [[SpeechPipeline alloc] init:google
+//                               configuration:_recognizerConfig
+//                                    delegate:self];
 }
 
 RCT_EXPORT_METHOD(start:(NSString*)foo)
 {
-  RCTLogInfo(@"Pretending to start");
+    RCTLogInfo(@"Pretending to start");
 }
 
 RCT_EXPORT_METHOD(stop)
 {
-  RCTLogInfo(@"Pretending to stop");
+    RCTLogInfo(@"Pretending to stop");
 }
 
 - (void)didFinish {
