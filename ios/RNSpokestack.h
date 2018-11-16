b49ada4f41086dc89f753a0a40b3727e7ccda109
Noel Weichbrodt <noel@weichbrodt.org>
2018-11-16 16:48:25 -0500

    removed Frameworks/Spokestack.framework; corrected RNSpokestack.h imports; minimal working RNSpokestack.m

--- ios/RNSpokestack.h
+++ ios/RNSpokestack.h
@@ -1,12 +1,8 @@
 
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
 #import <React/RCTBridgeModule.h>
-#endif
 #import <React/RCTEventEmitter.h>
-#import "SpokeStack/SpokeStack-Swift.h"
+#import <SpokeStack/SpokeStack-Swift.h>
 
 @interface RNSpokestack : RCTEventEmitter <RCTBridgeModule, SpeechRecognizer>
-
+//NSObject <RCTBridgeModule, SpeechRecognizer>
 @end
--- ios/RNSpokestack.m
+++ ios/RNSpokestack.m
@@ -1,7 +1,7 @@
 
 #import "RNSpokestack.h"
 #import <React/RCTLog.h>
-#import <SpokeStack/SpokeStack.h>
+#import <SpokeStack/SpokeStack-Swift.h>
 
 @implementation RNSpokestack
 {
@@ -26,7 +26,7 @@ RCT_EXPORT_MODULE();
 
 - (NSArray<NSString *> *)supportedEvents
 {
-    return @[onSpeechRecognized, onSpeechStarted, onSpeechEnded];
+    return @[@"onSpeechRecognized", @"onSpeechStarted", @"onSpeechEnded"];
 }
 
 SpeechPipeline* _pipeline;
@@ -34,51 +34,52 @@ SpeechPipeline* _pipeline;
 RCT_EXPORT_METHOD(initialize:(NSDictionary *)config)
 {
     RCTLogInfo(@"Pretending to initialize with config %@", config);
-    GoogleRecognizerConfiguration *_recognizerConfig = [[GoogleRecognizerConfiguration alloc] init];
-    _recognizerConfig.host = config["host"];
-    _recognizerConfig.enableWordTimeOffsets = config["enableWordTimeOffsets"];
-    _recognizerConfig.singleUtterance = config["singleUtterance"];
-    _recognizerConfig.maxAlternatives = config["maxAlternatives"];
-    _recognizerConfig.interimResults = config["interimResults"];
-    _recognizerConfig.apiKey = config["apiKey"];
-    _pipeline = [[SpeechPipeline alloc] init:google
-                               configuration:_recognizerConfig
-                                    delegate:self];
+//    GoogleRecognizerConfiguration *_recognizerConfig = [[GoogleRecognizerConfiguration alloc] init];
+//    _recognizerConfig.host = config[@"host"];
+//    _recognizerConfig.enableWordTimeOffsets = config[@"enableWordTimeOffsets"];
+//    _recognizerConfig.singleUtterance = config[@"singleUtterance"];
+//    _recognizerConfig.maxAlternatives = config[@"maxAlternatives"];
+//    _recognizerConfig.interimResults = config[@"interimResults"];
+//    _recognizerConfig.apiKey = config[@"apiKey"];
+//    _pipeline = [[SpeechPipeline alloc] init:google
+//                               configuration:_recognizerConfig
+//                                    delegate:self];
 }
 
 RCT_EXPORT_METHOD(start:(NSString*)foo)
 {
     RCTLogInfo(@"Pretending to start");
-    _pipeline.start();
+    //_pipeline.start();
 }
 
 RCT_EXPORT_METHOD(stop)
 {
     RCTLogInfo(@"Pretending to stop");
-    _pipeline.stop();
+    //_pipeline.stop();
 }
 
 - (void)didFinish {
     RCTLogInfo(@"Pretending speech finished");
     if (hasListeners)
     {
-        [self sendEventWithName:onSpeechEnded];
+        //[self sendEventWithName:@"onSpeechEnded"];
     }
 }
 
 - (void)didRecognize:(SPSpeechContext * _Nonnull)results {
-    RCTLogInfo(@"Pretending speech recognized as %@", result.transcript);
+    //RCTLogInfo(@"Pretending speech recognized as %@", result.transcript);
     if (hasListeners)
     {
-        [self sendEventWithName:onSpeechRecognized body:@{@"transcript": result.transcript}];
+        //[self sendEventWithName:@"onSpeechRecognized" body:@{@"transcript": result.transcript}];
+        
     }
-}
+}
 
 - (void)didStart {
     RCTLogInfo(@"Pretending speech started");
     if (hasListeners)
     {
-        [self sendEventWithName:onSpeechStarted];
+        //[self sendEventWithName:@"onSpeechStarted"];
     }
 }
 
